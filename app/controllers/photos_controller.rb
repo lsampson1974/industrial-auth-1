3ca1bab2986a06313c2e37322a65a8be12164db5
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-03-12 15:46:00 +0000

    Corrections to authorizations

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -51,11 +51,20 @@ class PhotosController < ApplicationController
 
   # DELETE /photos/1 or /photos/1.json
   def destroy
-    @photo.destroy
-    respond_to do |format|
-      format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
-      format.json { head :no_content }
+
+    if current_user == @photo.owner
+      @photo.destroy
+      
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
+        format.json { head :no_content }
+      end
+
+    else
+
+      redirect_back(fallback_location: root_url, notice: "Nice try, but that is not your photo.")
     end
+
   end
 
   private
